4aa5a2ca8f37f557bee9e98c2af78cfb3344c03e
Prakash <prakaships@outlook.com>
2026-07-07 09:10:03 +0000

    Created constant file for the project

---
+++ client/src/constants/realtimeEvents.js
@@ -0,0 +1,6 @@
+export const REALTIME_EVENTS = Object.freeze({
+    messageSend: 'message:send',
+    messageReceive: 'message:receive',
+    userOnline: 'user:online',
+    userOffline: 'user:offline',
+})
